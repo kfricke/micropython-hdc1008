6ea7a062ff7148e0d43e47824fd97545abd3cf8b
Kai Fricke <kai@kai-ws-2>
2016-05-14 08:44:43 +0200

    Overall cleanup and update to new hardware API. * Removed docstrings * Refactored constant names * Overall code size optimization * Unified ans implified API (no getter/setter, but attributes)

--- hdc1008.py
+++ hdc1008.py
@@ -1,215 +1,124 @@
 from machine import I2C
-from time import sleep_ms
+from utime import sleep_ms
 
-R_TEMP    = const(0x00)
-R_HUMI    = const(0x01)
-R_CFG  = const(0x02)
-R_SER_ID1 = const(0xfb)
-R_SER_ID2 = const(0xfc)
-R_SER_ID3 = const(0xfd)
+REG_TEMP = const(0x00)
+REG_HUMI = const(0x01)
+REG_CFG = const(0x02)
+REG_SER_ID1 = const(0xfb)
+REG_SER_ID2 = const(0xfc)
+REG_SER_ID3 = const(0xfd)
 
-C_RST         = const(0b10000000)
-C_HEAT_ON     = const(0b00100000)
-C_HEAT_OFF    = const(0b00000000)
-C_MODE_SINGLE = const(0b00000000)
-C_MODE_BOTH   = const(0b00010000)
-C_BTST        = const(0b00001000)
-C_TEMP_14BIT  = const(0b00000000)
-C_TEMP_11BIT  = const(0b00000100)
-C_HUMI_14BIT  = const(0b00000000)
-C_HUMI_11BIT  = const(0b00000001)
-C_HUMI_8BIT   = const(0b00000010)
+CFG_RST = const(0b10000000)
+CFG_HEAT_ON = const(0b100000)
+CFG_HEAT_OFF = const(0b0)
+CFG_MODE_SINGLE = const(0b0)
+CFG_MODE_BOTH = const(0b10000)
+CFG_BTST = const(0b1000)
+CFG_TEMP_14BIT = const(0b0)
+CFG_TEMP_11BIT = const(0b100)
+CFG_HUMI_14BIT = const(0b0)
+CFG_HUMI_11BIT = const(0b1)
+CFG_HUMI_8BIT = const(0b10)
 
 RES_8_BIT  = const(1)
 RES_11_BIT = const(2)
 RES_14_BIT = const(3)
 
 class HDC1008(object):
-    """
-    This class implements access to an Texas Instruments HDC1008 humidity and
-    Temperature sensor.
-    """
-
-    def __init__(self, i2c, addr=0x40):
-        """
-        Initialize the sensor. The I2C bus for the sensor can be specified by 
-        its logical number or as an pre-initialized object.
-        """
+    def __init__(self, i2c, addr=0x40, heater=False):
         if i2c == None or i2c.__class__ != I2C:
             raise ValueError('I2C object needed as argument!')
         self._i2c = i2c
         self._addr = addr
-        self._heater = False
+        self._heater = heater
+        self._tmp = bytearray(4)
+        # Read the sensor serial ID
+        self.serial = 0
+        self._send_byte(REG_SER_ID1)
+        self._recv(self._tmp, 2)
+        self.serial += (self._tmp[0] << 32) + (self._tmp[1] << 24)
+        self._send_byte(REG_SER_ID2)
+        self._recv(self._tmp, 2)
+        self.serial += (self._tmp[0] << 16) + (self._tmp[1] << 8)
+        self._send_byte(REG_SER_ID3)
+        self._recv(self._tmp, 2)
+        self.serial += self._tmp[0]
 
-    def _send(self, buf):
-        """
-        Sends the given bufer object over I2C to the sensor.
-        """
-        self._i2c.send(buf, self._addr)
+    def _send_byte(self, b):
+        self._tmp[0] = b
+        self._i2c.writeto(self._addr, self._tmp[0:1])
+
+    def _send_bytes(self, b):
+        self._i2c.writeto(self._addr, b)
         
-    def _recv(self, count):
-        """
-        Read bytes from the sensor using I2C. The byte count can be specified 
-        as an argument.
-        """
-        buf = bytearray(count)
-        self._i2c.recv(buf, self._addr)
-        return buf
+    def _recv(self, buf, c):
+        buf[0:c] = self._i2c.readfrom(self._addr, c)
 
     def _config(self, reg=None):
-        """
-        Set the given config register to configure the sensor.
-        """
-        if reg == None:
-            self._send(R_CFG)
-            return self._recv()
+        if reg is None:
+            self._send_byte(REG_CFG)
+            self._recv(self._tmp, 2)
+            return self._tmp[0:2]
         else:
             if self._heater:
-                reg = reg | C_HEAT_ON
-            buf = bytearray(3)
-            buf[0] = R_CFG
-            buf[1] = reg
-            self._send(buf)
+                reg = reg | CFG_HEAT_ON
+            self._tmp[0] = REG_CFG
+            self._tmp[1] = reg
+            self._tmp[2] = 0 
+            self._send_bytes(self._tmp[0:3])
 
-    def get_heater(self):
-        """
-        Returns the current state of the heater.
-        """
-        return self._heater
+    def heater(self, s=None):
+        if s is not None:
+            if s.__class__ != bool:
+                raise ValueError('Heater state must be a boolean value or None!')
+            else:
+                self._heater = s
+        else:
+            return self._heater
 
-    def set_heater(self, s):
-        """
-        Sets the heater status for measurements. 
-        """
-        if s.__class__ not bool:
-            raise ValueError('Heater state must be a boolean value or None!')
-        self._heater = s
-
-    def get_battery_low(self):
-        """
-        Reads the battery status from the config register of the sensor. In 
-        case the input voltage drops below 2.7 V this method returns True.
-        """
-        raw = self._raw_temp()
-        return (self._config()[0] & C_BTST) == C_BTST
+    def battery_low(self):
+        # one read command needed to have the config register defined, where 
+        # we can read this flag from
+        self._raw_temp(CFG_TEMP_11BIT)
+        return (self._config()[0] & CFG_BTST) == CFG_BTST
 
     def reset(self):
-        """
-        Perform a software reset of the sensor.
-        """
-        buf = bytearray(3)
-        buf[0] = R_CFG
-        buf[1] = C_RST
-        self._send(buf)
+        self._tmp[0] = REG_CFG
+        self._tmp[1] = CFG_RST
+        self._tmp[2] = 0 
+        self._send_bytes(self._tmp[0:3])
         sleep_ms(20)
 
-    def _raw_temp(self, acc=RES_14_BIT):
-        """
-        Read only the temperature from the seonsor. The accuracy can be 
-        specified and is valid for 11 and 14 bits.
-        """
-        if not acc in (RES_11_BIT, RES_14_BIT):
-            raise ValueError('Temperature measurement resolution invalid!')
-        config = C_MODE_SINGLE
-        if acc == RES_11_BIT:
-            config = config | C_TEMP_11BIT
-        elif acc == RES_14_BIT:
-            config = config | C_TEMP_14BIT
-        self._config(config)
-        self._send(R_TEMP)
+    def _raw_temp(self, acc):
+        self._config(CFG_MODE_SINGLE | acc)
+        self._send_byte(REG_TEMP)
         sleep_ms(15)
-        raw = self._recv()
-        return raw[1] + (raw[0] << 8)
+        self._recv(self._tmp, 2)
+        return self._tmp[1] + (self._tmp[0] << 8)
         
-    def get_temp(self, acc=14):
-        """
-        Reads the temperature and returns degree in celsius. Use the given 
-        accuracy in bits for the sensor reading. Valid accuracy values are 
-        8, 11 and 14.
-        """
-        if not acc in (RES_11_BIT, RES_14_BIT):
-            raise ValueError('Temperature measurement resolution invalid!')
-        return (self._raw_temp(acc) / 65536.0) * 165.0 - 40.0
+    def temp(self, acc=CFG_TEMP_14BIT):
+        return (self._raw_temp(acc) / 65536) * 165 - 40
         
-    def _raw_humi(self, acc=RES_14_BIT):
-        """
-        Reads the raw humidity value from the seonsor.
-        Returns an integer value.
-        """
-        if not acc in (RES_8_BIT, RES_11_BIT, RES_14_BIT):
-            raise ValueError('Humidity measurement resolution invalid!')
-        config = C_MODE_SINGLE
-        if acc == RES_8_BIT:
-            config = config | C_HUMI_8BIT
-        elif acc == RES_11_BIT:
-            config = config | C_HUMI_11BIT
-        elif acc == RES_14_BIT:
-            config = config | C_HUMI_14BIT
-        self._config(config)
-        self._send(R_HUMI)
+    def _raw_humi(self, acc):
+        self._config(CFG_MODE_SINGLE | acc)
+        self._send_byte(REG_HUMI)
         sleep_ms(13)
-        raw = self._recv()
-        return raw[1] + (raw[0] << 8)
+        self._recv(self._tmp, 2)
+        return self._tmp[1] + (self._tmp[0] << 8)
         
-    def get_humi(self, acc=14):
-        """
-        Return the relative humidity in percent. Use the given accuracy in 
-        bits for the sensor reading. Valid accuracy values are 8, 11 and 14.
-        """
-        if not acc in (RES_8_BIT, RES_11_BIT, RES_14_BIT):
-            raise ValueError('Humidity measurement resolution invalid!')
+    def humi(self, acc=CFG_HUMI_14BIT):
         return (self._raw_humi(acc) / 65536.0) * 100.0
 
-    def _raw_temp_humi(self, temp_acc=RES_14_BIT, humi_acc=RES_14_BIT):
-        """
-        Initiate a temperature and humidity sensor reading in one operation. 
-        This safes time (and thereby energy). Accuracy for both sensors can 
-        be specified. Same valid accuracy values apply to both seonsors.
-        """
-        if not temp_acc in (RES_11_BIT, RES_14_BIT):
-            raise ValueError('Temperature measure accuracy invalid!')
-        if not temp_acc in (RES_8_BIT, RES_11_BIT, RES_14_BIT):
-            raise ValueError('Humidity measure accuracy invalid!')
-        config = C_MODE_BOTH
-        if temp_acc == RES_11_BIT:
-            config = config | C_TEMP_11BIT
-        elif temp_acc == RES_14_BIT:
-            config = config | C_TEMP_14BIT
-        if humi_acc == RES_8_BIT:
-            config = config | C_HUMI_8BIT
-        elif humi_acc == RES_11_BIT:
-            config = config | C_HUMI_11BIT
-        elif humi_acc == RES_14_BIT:
-            config = config | C_HUMI_14BIT
-        self._config(config)
-        self._send(R_TEMP)
+    def _raw_temp_humi(self, t_acc, h_acc):
+        self._config(CFG_MODE_BOTH | t_acc | h_acc)
+        self._send_byte(REG_TEMP)
         sleep_ms(20)
-        raw = self._recv(4)
-        return (raw[1] + (raw[0] << 8), raw[3] + (raw[2] << 8))
+        self._recv(self._tmp, 4)
+        return (self._tmp[1] + (self._tmp[0] << 8), self._tmp[3] + (self._tmp[2] << 8))
 
-    def get_temp_humi(self, temp_acc=RES_14_BIT, humi_acc=RES_14_BIT):
-        """
-        Initiate a temperature and humidity sensor reading in one 
-        operation. This safes time (and thereby energy). Accuracy for 
-        both sensors can be specified. Same valid accuracy values apply to
-        both seonsors.
-        """
-        if not temp_acc in (RES_11_BIT, RES_14_BIT):
-            raise ValueError('Temperature measurement accuracy invalid!')
-        if not temp_acc in (RES_8_BIT, RES_11_BIT, RES_14_BIT):
-            raise ValueError('Humidity measurement accuracy invalid!')
-        (raw_temp, raw_humi) = self._raw_temp_humi(temp_acc, humi_acc)
+    def temp_humi(self, t_acc=CFG_TEMP_14BIT, h_acc=CFG_HUMI_14BIT):
+        (raw_temp, raw_humi) = self._raw_temp_humi(t_acc, h_acc)
         return (((raw_temp / 65536) * 165) - 40, (raw_humi / 65536) * 100.0)
 
-    def get_serial(self):
-        """
-        Retrieve the serial number of the sensor.
-        """
-        self._send(R_SER_ID1)
-        raw_1 = self._recv(2)
-        self._send(R_SER_ID2)
-        raw_2 = self._recv(2)
-        self._send(R_SER_ID3)
-        raw_3 = self._recv(2)
-        return (raw_1[0] << 32) + (raw_1[1] << 24) + (raw_2[0] << 16) + 
-               (raw_2[1] << 8) + raw_3[0]
+    def serial(self):
+        return self.serial
